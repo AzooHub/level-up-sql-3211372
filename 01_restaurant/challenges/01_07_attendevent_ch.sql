f58b3da97a15f4edeb2b60e32c54e5838e8078ed
AzooHub <153978991+AzooHub@users.noreply.github.com>
2024-01-18 12:34:03 +0300

    hh

--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -2,3 +2,8 @@
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+insert into  anniversary value ( 
+  Select CustomerID,PartySize
+   from customers join reservations
+   where Email ='atapley2j@kinetecoinc.com';
+   );
